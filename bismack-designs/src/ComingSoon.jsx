c0c88ef5c36e4f995cb66b8378df35b3f956e999
PatrickBismack <patrickbismack@oakland.edu>
2026-06-28 08:39:34 -0400

    Initial Commit

---
+++ bismack-designs/src/ComingSoon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ComingSoon.css";
+function ComingSoon() {
+  return (
+    <div className="container">
+      <div className="text">
+        <h1>Bismack Designs</h1>
+        <h2>A new look is on its way...</h2>
+      </div>
+    </div>
+  );
+}
+export default ComingSoon;
